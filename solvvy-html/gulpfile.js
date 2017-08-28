c040fe70c646c45c5c8b32e6eff408c1169c42cc
matiasjaubet <mjaubet@gmail.com>
2017-08-28 13:05:40 -0300

    First commit

---
+++ solvvy-html/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer'); 
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./",
+        port: 9000,
+        browser: "google chrome"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+
+});
+
+gulp.task('autoprefixer', () =>
+    gulp.src('scss/**/*.scss')
+        .pipe(gulp.dest('/scss'))
+);
+
+
+gulp.task('sass', function() {
+	return gulp.src('scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(sass())
+	.pipe(autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
+	.pipe(gulp.dest('./'))
+	.pipe(browserSync.stream());
+});
+
+
+
+// gulp.task('watch', function() {
+// 	gulp.watch('scss/**/*.scss',['sass']);
+// });
+
+
+
---
+++ solvvy-html/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	//PRELOADER
+
+	window.onload = function(){
+		$("#loader").fadeOut(350,function(){$(this).remove()});
+	};
+
+});
