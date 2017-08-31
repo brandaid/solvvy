13b31d74fed98f5d8871ca6695574c05cbecb7dc
HernanPereira <hp.hernanpereira@gmail.com>
2017-08-31 16:13:09 -0300

    commit footer

--- wordpress/wp-content/themes/solvvy/footer.php
+++ wordpress/wp-content/themes/solvvy/footer.php
@@ -1,6 +1,70 @@
 	</main>
 	<footer class="footer">
-		<div class="container">footer</div>
+
+		<div class="container col-brand">
+			<div class="col">
+				<img src="<?php bloginfo('template_url'); ?>/images/brand.png" alt="">
+			</div>
+		</div>
+
+		<div class="container links">
+			<div class="col col-1">
+				<h4>Solutions</h4>
+				<ul>
+					<li><a href="">For Your Role</a></li>
+					<li><a href="">For Your Business Type</a></li>
+					<li><a href="">For Your Industry</a></li>
+				</ul>
+			</div>
+			<div class="col col-2">
+				<h4><a href="">Software</a></h4>
+				<h4><a href="">Customers</a></h4>
+				<h4><a href="">Resources</a></h4>
+				<h4><a href="">Blog</a></h4>
+			</div>
+			<div class="col col-3">
+				<h4><a href="">Company</a></h4>
+				<ul>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Careers</a></li>
+					<li><a href="">News & Events</a></li>
+				</ul>
+			</div>
+			<div class="col col-4">
+				<h4><a href="">Get in Touch</a></h4>
+				<ul>
+					<li><a href="">Contact Us</a></li>
+					<li><a href="">Support</a></li>
+				</ul>				
+			</div>
+			<div class="col col-5">
+				<div><a href="" class="button">LIVE DEMO</a></div>
+				<div><a href="" class="button white">PRICING</a></div>
+			</div>
+			<div class="col col-6">
+				<h4>Our Office</h4>
+				<p>
+					425 Sherman Ave.<br>
+					Palo Alto, CA 94301<br>
+					(650) 246-9685<br>
+					<a href="mailto:info@solvvy.com">info@solvvy.com</a>
+				</p>
+				<div class="social">
+					<a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					<a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="sub-footer">
+			<div class="container">
+				<div class="col">uno</div>
+				<div class="col">uno</div>
+				<div class="col">uno</div>
+			</div>	
+		</div>
+
 	</footer>
 	<!--JQUERY -->
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.0.min.js"></script>
