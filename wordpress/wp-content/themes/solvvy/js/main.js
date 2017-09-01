ec20a6410fc21de2d70ace6e4e160cebd3acd393
HernanPereira <hp.hernanpereira@gmail.com>
2017-09-01 16:58:13 -0300

    footer

--- wordpress/wp-content/themes/solvvy/js/main.js
+++ wordpress/wp-content/themes/solvvy/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+		//SCROLL SUBIR
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
 	//PRELOADER
 
 	window.onload = function(){
