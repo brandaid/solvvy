5b91f95a3a3233892041fa30e3195969c391ec6f
matiasjaubet <mjaubet@gmail.com>
2017-09-01 17:49:22 -0300

    box map

--- wordpress/wp-content/themes/solvvy/index.php
+++ wordpress/wp-content/themes/solvvy/index.php
@@ -326,7 +326,7 @@
 
 		<section class="box-map">
 			<div class="container">
-				<h2>Join the Thousands of Companies Using Solvvy</h2>
+				<h2>Join The Thousands of Companies Using Solvvy</h2>
 				<p>Signing up is fast, free and easy. Call us today at (650) 246-9685.</p>
 				<a href="" class="button">Button</a>
 				<a href="" class="button button-secondary">Button</a>
