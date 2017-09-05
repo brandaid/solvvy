631bdbbab676303d8e0dd7e7d7edac8d8fc1cf73
matiasjaubet <mjaubet@gmail.com>
2017-09-05 15:53:06 -0300

    main.js fixed

--- wordpress/wp-content/themes/solvvy/js/main.js
+++ wordpress/wp-content/themes/solvvy/js/main.js
@@ -112,6 +112,20 @@ $(document).ready(function() {
 	        video.src = document.getElementById('video').src;
 	    }
 	}
+
+
+	$('.center').slick({
+    centerMode: true,
+    centerPadding: '10px',
+    slidesToShow: 3,
+    speed: 1000,
+    autoplay: true,
+		autoplaySpeed: 4000,
+    vertical: true,
+    arrows: false
+  });
+
+
 });
 
 
@@ -155,22 +169,8 @@ $(function() {
     });
   }
 
-});
-
-
-$('.center').slick({
-        centerMode: true,
-        centerPadding: '10px',
-        slidesToShow: 3,
-        speed: 1000,
-        autoplay: true,
-  		autoplaySpeed: 4000,
-        vertical: true,
-        arrows: false
-      });
-
-
-	var $menuTop = $('.header-top ul li').clone();
+  var $menuTop = $('.header-top ul li').clone();
 	$('.menu-top-clone').html($menuTop);
 });
+
 
