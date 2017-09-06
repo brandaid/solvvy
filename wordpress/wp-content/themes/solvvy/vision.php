246076b1083d288fed9f2a29b00eef2ef89e1f9d
matiasjaubet <mjaubet@gmail.com>
2017-09-06 14:25:16 -0300

    Vision page QA

--- wordpress/wp-content/themes/solvvy/vision.php
+++ wordpress/wp-content/themes/solvvy/vision.php
@@ -116,114 +116,116 @@
 
 		<div class="cool-sep"></div>
 
-		<div class="container">
-			<div class="wrapperTable">
-				<div class="contTr contTrHeader">
-					<div class="contTdOne contTdOneHeader"></div>
-					<div class="contTdTwo contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand.png" alt=""></div>
-					<div class="contTdThree contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-zendesk.png" alt=""></div>
-					<div class="contTdFour contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-upwork.png" alt=""></div>
-					<div class="contTdFive contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-mailchimp.png" alt=""></div>
-					<div class="contTdSix contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-task.png" alt=""></div>
-					<div class="contTdSeven contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-double.png" alt=""></div>
-					<div class="contTdEight contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-woo.png" alt=""></div>
+		<section class="box-text-top container-normal align-center">
+			<div class="container">
+				<h2 class="margin-bottom-90">Competitive Matrix</h2>
+
+				<div class="wrapperTable">
+					<div class="contTr contTrHeader">
+						<div class="contTdOne contTdOneHeader"></div>
+						<div class="contTdTwo contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand.png" alt=""></div>
+						<div class="contTdThree contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-zendesk.png" alt=""></div>
+						<div class="contTdFour contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-upwork.png" alt=""></div>
+						<div class="contTdFive contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-mailchimp.png" alt=""></div>
+						<div class="contTdSix contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-task.png" alt=""></div>
+						<div class="contTdSeven contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-double.png" alt=""></div>
+						<div class="contTdEight contTdTwoHeaders"><img src="<?php bloginfo('template_url'); ?>/images/brand-woo.png" alt=""></div>
+					</div>
+				</div>
+				<div class="wrapperTable tableShadow">	
+
+					<h3 class="title-head">Machine Learning</h3>			
+					
+					<div class="contTr">
+						<div class="contTdOne">Machine Learning</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"></div>
+						<div class="contTdFour"></div>
+						<div class="contTdFive"><i class="fa fa-check"></i></div>
+						<div class="contTdSix"><i class="fa fa-check"></i></div>
+						<div class="contTdSeven"></div>
+						<div class="contTdEight"></div>
+					</div>
+					
+					<h3 class="title-head">Artificial Intelligence</h3>
+				
+					<div class="contTr">
+						<div class="contTdOne">Artificial Intelligence</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"></div>
+						<div class="contTdFour"><i class="fa fa-check"></i></div>
+						<div class="contTdFive"></div>
+						<div class="contTdSix"><i class="fa fa-check"></i></div>
+						<div class="contTdSeven"></div>
+						<div class="contTdEight"><i class="fa fa-check"></i></div>
+					</div>
+					
+					<h3 class="title-head">Transparent Metrics</h3>
+				
+					<div class="contTr">
+						<div class="contTdOne">Transparent Metrics</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"><i class="fa fa-check"></i></div>
+						<div class="contTdFour"></div>
+						<div class="contTdFive"></div>
+						<div class="contTdSix"></div>
+						<div class="contTdSeven"><i class="fa fa-check"></i></div>
+						<div class="contTdEight"><i class="fa fa-check"></i></div>
+					</div>
+
+					<h3 class="title-head">Advanced Science</h3>
+
+					<div class="contTr">
+						<div class="contTdOne">Advanced Science</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"><i class="fa fa-check"></i></div>
+						<div class="contTdFour"></div>
+						<div class="contTdFive"><i class="fa fa-check"></i></div>
+						<div class="contTdSix"></div>
+						<div class="contTdSeven"></div>
+						<div class="contTdEight"></div>
+					</div>
+
+					<h3 class="title-head">Machine Learning</h3>
+
+					<div class="contTr">
+						<div class="contTdOne">Machine Learning</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"></div>	
+						<div class="contTdFour"><i class="fa fa-check"></i></div>
+						<div class="contTdFive"></div>
+						<div class="contTdSix"><i class="fa fa-check"></i></div>
+						<div class="contTdSeven"></div>
+						<div class="contTdEight"><i class="fa fa-check"></i></div>
+					</div>
+					
+					<h3 class="title-head">Artificial Intelligence</h3>
+
+					<div class="contTr">
+						<div class="contTdOne">Artificial Intelligence</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"><i class="fa fa-check"></i></div>
+						<div class="contTdFour"></div>
+						<div class="contTdFive"><i class="fa fa-check"></i></div>
+						<div class="contTdSix"></div>
+						<div class="contTdSeven"><i class="fa fa-check"></i></div>
+						<div class="contTdEight"></div>
+					</div>
+
+					<h3 class="title-head">Transparent Metrics</h3>
+
+					<div class="contTr">
+						<div class="contTdOne">Transparent Metrics</div>
+						<div class="contTdTwo"><i class="fa fa-check"></i></div>
+						<div class="contTdThree"></div>
+						<div class="contTdFour"><i class="fa fa-check"></i></div>
+						<div class="contTdFive"></div>
+						<div class="contTdSix"><i class="fa fa-check"></i></div>
+						<div class="contTdSeven"><i class="fa fa-check"></i></div>
+						<div class="contTdEight"></div>
+					</div>
+	
 				</div>
 			</div>
-			<div class="wrapperTable tableShadow">	
-
-				<h3 class="title-head">Machine Learning</h3>			
-				
-				<div class="contTr">
-					<div class="contTdOne">Machine Learning</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"></div>
-					<div class="contTdFour"></div>
-					<div class="contTdFive"><i class="fa fa-check"></i></div>
-					<div class="contTdSix"><i class="fa fa-check"></i></div>
-					<div class="contTdSeven"></div>
-					<div class="contTdEight"></div>
-				</div>
-				
-				<h3 class="title-head">Artificial Intelligence</h3>
-			
-				<div class="contTr">
-					<div class="contTdOne">Artificial Intelligence</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"></div>
-					<div class="contTdFour"><i class="fa fa-check"></i></div>
-					<div class="contTdFive"></div>
-					<div class="contTdSix"><i class="fa fa-check"></i></div>
-					<div class="contTdSeven"></div>
-					<div class="contTdEight"><i class="fa fa-check"></i></div>
-				</div>
-				
-				<h3 class="title-head">Transparent Metrics</h3>
-			
-				<div class="contTr">
-					<div class="contTdOne">Transparent Metrics</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"><i class="fa fa-check"></i></div>
-					<div class="contTdFour"></div>
-					<div class="contTdFive"></div>
-					<div class="contTdSix"></div>
-					<div class="contTdSeven"><i class="fa fa-check"></i></div>
-					<div class="contTdEight"><i class="fa fa-check"></i></div>
-				</div>
-
-				<h3 class="title-head">Advanced Science</h3>
-
-				<div class="contTr">
-					<div class="contTdOne">Advanced Science</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"><i class="fa fa-check"></i></div>
-					<div class="contTdFour"></div>
-					<div class="contTdFive"><i class="fa fa-check"></i></div>
-					<div class="contTdSix"></div>
-					<div class="contTdSeven"></div>
-					<div class="contTdEight"></div>
-				</div>
-
-				<h3 class="title-head">Machine Learning</h3>
-
-				<div class="contTr">
-					<div class="contTdOne">Machine Learning</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"></div>	
-					<div class="contTdFour"><i class="fa fa-check"></i></div>
-					<div class="contTdFive"></div>
-					<div class="contTdSix"><i class="fa fa-check"></i></div>
-					<div class="contTdSeven"></div>
-					<div class="contTdEight"><i class="fa fa-check"></i></div>
-				</div>
-				
-				<h3 class="title-head">Artificial Intelligence</h3>
-
-				<div class="contTr">
-					<div class="contTdOne">Artificial Intelligence</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"><i class="fa fa-check"></i></div>
-					<div class="contTdFour"></div>
-					<div class="contTdFive"><i class="fa fa-check"></i></div>
-					<div class="contTdSix"></div>
-					<div class="contTdSeven"><i class="fa fa-check"></i></div>
-					<div class="contTdEight"></div>
-				</div>
-
-				<h3 class="title-head">Transparent Metrics</h3>
-
-				<div class="contTr">
-					<div class="contTdOne">Transparent Metrics</div>
-					<div class="contTdTwo"><i class="fa fa-check"></i></div>
-					<div class="contTdThree"></div>
-					<div class="contTdFour"><i class="fa fa-check"></i></div>
-					<div class="contTdFive"></div>
-					<div class="contTdSix"><i class="fa fa-check"></i></div>
-					<div class="contTdSeven"><i class="fa fa-check"></i></div>
-					<div class="contTdEight"></div>
-				</div>
-				
-			</div>
-		</div>
-
+		</section>
 <?php get_footer(); ?>
-
