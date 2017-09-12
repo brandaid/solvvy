acd16429701b4e7e0719f69f8a173c62c8cb0389
gacierno <german@thebytery.com>
2017-09-12 15:40:53 -0300

    Gulp file 4

--- wordpress/build-files/gulpfile.js
+++ wordpress/build-files/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer'); 
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 
 gulp.task('sass', function(){
@@ -11,6 +13,26 @@ gulp.task('sass', function(){
                     browsers: ['last 10 versions'],
                     cascade: false
                }))
-               .pipe(gulp.dest('../wp-content/themes/solvvy/'));
+               .pipe(gulp.dest('../wp-content/themes/solvvy/'))
+               .pipe(reload({stream:true}));
 });
 
+gulp.task('browser-sync', function(){
+    var files = [
+        './*.css',
+        '../**/*.js',
+        '../**/*.php'
+    ];
+
+    browserSync.init(files, {
+        proxy: 'localhost:8888',
+        browser: 'google chrome',
+        notify: true
+    });
+});
+
+
+gulp.task('default', ['sass', 'browser-sync'], function(){
+    console.log('Run server');
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
