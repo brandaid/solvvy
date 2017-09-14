7fbc05194754b5729a5e3395d67e7b9eb51f6030
matiasjaubet <mjaubet@gmail.com>
2017-09-14 14:03:12 -0300

    Spheres

--- wordpress/wp-content/themes/solvvy/careers.php
+++ wordpress/wp-content/themes/solvvy/careers.php
@@ -254,13 +254,14 @@
 
 		<div class="cool-sep"></div>
 
-		<!-- SPHERES PETER -->
+		<!-- SPHERES -->
 
 		<section class="box-text-top align-center container-normal bkg-none">
 			<div class="container">
+				<p>22</p>
 				<h2>By The Numbers</h2>
 				<p>Bacon ipsum dolor amet flank turkey sausage, brisket meatball beef ribs chicken. Cow meatloaf <br> short ribs, tail pork loin filet mignon leberkas chicken burgdoggen tenderloin brisket shankle. </p>
-				<div>[SPHERES PETER]</div>
+				<div id="cy"></div>
 			</div>
 		</section>
 
