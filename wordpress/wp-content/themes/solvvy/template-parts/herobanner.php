680a7c6b2a13a7ed562f2568027e9ace0251f02a
matiasjaubet <mjaubet@gmail.com>
2017-09-15 15:37:21 -0300

    Featured image on hero image

--- wordpress/wp-content/themes/solvvy/template-parts/herobanner.php
+++ wordpress/wp-content/themes/solvvy/template-parts/herobanner.php
@@ -1,15 +1,18 @@
 		<!-- SECTION HERO BANNER -->
 		
 		<?php $section_hero = get_field('section_hero') ?>
+		
+		<?php
+			$image_id = get_field('hero_background');
+			$image_size = 'featured';
+			$image_array = wp_get_attachment_image_src($image_id, $image_size);
+			$image_url = $image_array[0];
+		?>
+
+		
 
 		<?php if($section_hero){ ?>
 			<?php if($section_hero['hero_background']){ ?>
-			<?php
-				$image_id = $section_hero['hero_background'];
-				$image_size = 'featured';
-				$image_array = wp_get_attachment_image_src($image_id, $image_size);
-				$image_url = $image_array[0];
-			?>
 			<section class="hero-banner hero-banner-interior" style="background-image: url(<?php echo $image_url; ?>)">
 			<?php }else{ ?>
 			<section class="hero-banner hero-banner-interior">
