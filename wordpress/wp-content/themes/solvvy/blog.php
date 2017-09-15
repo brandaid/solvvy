1ce68649bc6a2b13ecad771ae2307b3e2bf43a8d
matiasjaubet <mjaubet@gmail.com>
2017-09-15 18:56:34 -0300

    Blog page

--- wordpress/wp-content/themes/solvvy/blog.php
+++ wordpress/wp-content/themes/solvvy/blog.php
@@ -55,6 +55,45 @@
 							<figure class="avatar">
 								<img src="<?php bloginfo('template_url'); ?>/images/the-man-6.jpg" class="responsive" alt="">
 							</figure>
+							<h2><?php the_autor(); ?>Sophie Conti</h2>
+							<h3><?php the_field('position'); ?>CEO, Customer Service Lab </h3>
+							<p><?php the_field('bio'); ?>Sophie is the founder of Customer Service Lab, and she has spent her career working with startups in customer support with some of the most innovative companies like Leap Motion, Tile, Moo, Etsy, Indiegogo, Teespring, and more.</p>
+						</div>
+					</small>
+					<p><?php the_field('excerpt'); ?>Bacon ipsum dolor amet flank turkey sausage, brisket meatball beef ribs chicken. Cow meatloaf short ribs, tail pork loin filet mignon leberkas chicken burgdoggen tenderloin brisket shankle. Salami prosciutto leberkas picanha, boudin turkey fatback meatloaf shankle flank chuck. Porchetta pancetta rump capicola pork pastrami. Pastrami ground round capicola sirloin chicken turducken swine hamburger pig.</p>
+					<div class="hidden-info">
+						<img src="<?php bloginfo('template_url'); ?>/images/image-slider-brand.jpg" class="responsive" alt="">
+						<?php the_post_thumbnail( 'inner-post', array( 'class' => 'responsive' ) ); ?>
+						<?php the_field('copy'); ?>
+						<p>Bacon ipsum dolor amet flank turkey sausage, brisket meatball beef ribs chicken. Cow meatloaf short ribs, tail pork loin filet mignon leberkas chicken burgdoggen tenderloin brisket shankle. Salami prosciutto leberkas picanha, boudin turkey fatback meatloaf shankle flank chuck. Porchetta pancetta rump capicola pork pastrami. Pastrami ground round capicola sirloin chicken turducken swine hamburger pig.</p>
+						<h3>Lorem Ipsum dole sldaelay</h3>
+						<p>Bacon ipsum dolor amet flank turkey sausage, brisket meatball beef ribs chicken. Cow meatloaf short ribs, tail pork loin filet mignon leberkas chicken burgdoggen tenderloin brisket shankle. Salami prosciutto leberkas picanha, boudin turkey fatback meatloaf shankle flank chuck. Porchetta pancetta rump capicola pork pastrami. Pastrami ground round capicola sirloin chicken turducken swine hamburger pig.</p>
+						<h4>Lore upsumss</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad tenetur, ipsam fugit sit optio suscipit ratione, tempora corporis quam incidunt, molestiae architecto dicta quas. Facilis, incidunt, perferendis. Sapiente, est, veritatis.</p>
+						<blockquote>Molestiae architecto dicta quas.</blockquote>
+						<p>Bacon ipsum dolor amet flank turkey sausage, brisket meatball beef ribs chicken. Cow meatloaf short ribs, tail pork loin filet mignon leberkas chicken burgdoggen tenderloin brisket shankle.</p>
+					</div>
+					<a href="javascript:void(0);" class="button-more">READ MORE</a>
+
+					<svg class="waves-on-bottom" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1657 134.52"><title>wave-top</title><path d="M1657,173V92c-222.29,15.82-453.31,37.81-652.11,5.85C275.33-19.45,191.35,69.61,0,90.44v79.3L121.44,173Z" transform="translate(0 -38.45)" style="fill:#fe92bb;opacity:0.3499999940395355;isolation:isolate"/><path d="M1657,173V44.1c-183.6,31.36-380.1,76.73-532.61,88.19C752,160.27,403.57,4.84,0,64.52V173Z" transform="translate(0 -38.45)" style="fill:#fa3777"/></svg>
+				</div>
+
+
+
+				<!-- POST -->
+				
+				<div class="box-post with-waves-on-bottom">
+					<div class="content-avatar">
+						<figure class="avatar">
+							<img src="<?php bloginfo('template_url'); ?>/images/the-man-6.jpg" class="responsive" alt="">
+						</figure>
+					</div>
+					<h2>Lorem ipsum dolor sit amet</h2>
+					<small><a href="javascript:void(0);" class="link-author">Mathesh Ram</a> on October 6, 2017
+						<div class="popup-author">
+							<figure class="avatar">
+								<img src="<?php bloginfo('template_url'); ?>/images/the-man-6.jpg" class="responsive" alt="">
+							</figure>
 							<h2>Sophie Conti</h2>
 							<h3>CEO, Customer Service Lab </h3>
 							<p>Sophie is the founder of Customer Service Lab, and she has spent her career working with startups in customer support with some of the most innovative companies like Leap Motion, Tile, Moo, Etsy, Indiegogo, Teespring, and more.</p>
