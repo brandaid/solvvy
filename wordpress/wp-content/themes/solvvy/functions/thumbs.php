ddd983ab3eece426b17ed97b49818c2ee228c5b5
matiasjaubet <mjaubet@gmail.com>
2017-09-15 18:05:39 -0300

    Newsletter page

--- wordpress/wp-content/themes/solvvy/functions/thumbs.php
+++ wordpress/wp-content/themes/solvvy/functions/thumbs.php
@@ -15,6 +15,7 @@ if ( function_exists( 'add_image_size' ) ) {
     add_image_size( 'join_us_section', 360, 273, true );
     add_image_size( 'featured_resource_image', 268, 197, true );
     add_image_size( 'featured_events', 398, 135, true );
+    add_image_size( 'newsletter', 400, 175, true );
 }
 
 add_filter( 'image_size_names_choose', 'my_custom_sizes' );
@@ -33,6 +34,7 @@ function my_custom_sizes( $sizes ) {
 	    'join_us_section' => __('Section Join'),
 	    'featured_resource_image' => __('Section Resources'),
 	    'featured_events' => __('Section Events'),
+	    'newsletter' => __('Section Newsletter'),
 	) );
 }
 
