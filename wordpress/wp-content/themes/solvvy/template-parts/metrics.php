9fe95a58d16dfea04e7776bc99cbf01f99538cbf
gacierno <german@thebytery.com>
2017-09-18 10:00:58 -0300

    Connections for bubbles on metrics

--- wordpress/wp-content/themes/solvvy/template-parts/metrics.php
+++ wordpress/wp-content/themes/solvvy/template-parts/metrics.php
@@ -12,49 +12,64 @@
 <?php } ?>
 
 <script type="text/javascript">
-var elements = 10;
+var bubblesArray = <?php echo json_encode( $metrics['metrics_bubles'] ) ?>;
+console.log(bubblesArray);
+var elements = bubblesArray.length;
 var colors = ['#725AB0', '#D5CEFA', '#F6639A', '#5F108F'];
 var size = [1, 0.9, 0.8, 0.7, 0.6];
 
 var cy = cytoscape({
 	container: document.getElementById('cy'),
 	style: [
-{
-    selector: 'node',
-    style: {
-        shape: 'ellipse',
-        label: 'data(id)'
-    }
-}]
+		{
+		    selector: 'node',
+		    style: {
+		        shape: 'ellipse',
+		        label: 'data(id)'
+		    }
+		}]
 });
 for (var i = 0; i < elements; i++) {
-var size2 = 200 * size[Math.floor(Math.random()*size.length)];
-cy.add({
-	data: { id: 'node' + i },
-	style: { 
-		'background-color': colors[Math.floor(Math.random()*colors.length)],
-		'height' : size2 + 'px',
-		'width' : size2 + 'px',
-		'font-size' : size2/5 + 'px',
-		'color' : '#fff',
-		'text-valign' : 'center',
-	}
-});
+	var size2 = 120 * size[Math.floor(Math.random()*size.length)];
+	var aux = bubblesArray[i].bubble_title;
+	cy.add({
+		data: { id: aux },
+		style: { 
+			'background-color': colors[Math.floor(Math.random()*colors.length)],
+			'height' : size2 + 'px',
+			'width' : size2 + 'px',
+			'font-size' : size2/5 + 'px',
+			'color' : '#fff',
+			'text-valign' : 'center',
+		}
+	});
 }
 for (var i = 0; i < elements; i++) {
-var source = 'node' + i;
-do { var target = 'node' + (Math.floor(Math.random() * elements * 0.5) ); }
-while (source == target);
-cy.add({
-data: {
-    id: 'edge' + i,
-    source: source,
-    target: target
+	var source = bubblesArray[i].bubble_title;
+	console.log(bubblesArray[i].bubble_title);
+	
+	var target; 
+	for(var j = 0; j < elements ; j++){
+		if((j!= i)&&(Math.random() > 0.8)){
+			target = bubblesArray[j].bubble_title;
+			cy.add({
+			data: {
+			    id: 'edge' + i + '_' +j,
+			    source: source,
+			    target: target,
+			    style:{
+			    	'width' : 1,
+			    }
+			}
+			});
+		}
+
 	}
-});
+
+
 }
 var layout = cy.layout({
-	name: 'cose'
+	name: 'circle'
 });
 cy.nodes().ungrabify(); // disable node dragging
 cy.userZoomingEnabled( false );
