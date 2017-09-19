a5099e47a470e51e15c85975f0e8d4f59879c96a
Mariana Esteban <mariana@thebytery.com>
2017-09-19 19:23:08 -0300

    Solutions single

---
+++ wordpress/wp-content/themes/solvvy/single-solutions_cards.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Solvvy
+
+ Template Name: Template Solutions Subpage
+
+*/
+?>
+<?php get_header(); ?>
+<?php the_post(); ?>
+
+
+		<?php get_template_part( 'template-parts/herobanner' ); ?>
+
+
+		<!-- BOX TEXT -->
+		
+		<?php $section_1 = get_field('section_1') ?>
+		<?php if($section_1){ ?>
+		<section class="box-text-top pink-ball box-text-and-image-top">
+			<div class="container">
+				<div class="mackbook">
+					<div class="content-mac">
+						<img src="<?php bloginfo('template_url'); ?>/images/macbook.gif" alt="" class="responsive">
+					</div>
+				</div>
+				<h2><?php echo $section_1['section_1_title'];  ?></h2>
+				<p><?php echo $section_1['section_1_copy'];  ?></p>
+				<?php if($section_1['section_1_button_destination'] && $section_1['section_1_button_text']){ ?><a href="<?php echo $section_1['section_1_button_destination']; ?>" class="button"><?php echo $section_1['section_1_button_text']; ?></a><?php } ?>
+			</div>
+		</section>
+		<?php } ?>
+
+		<!-- VALUE TAGS -->
+		<?php $valueSection = get_field('value_driver_section');
+		$link = $valueSection['value_driver_section_button'];
+		 ?>
+		<?php if($valueSection){ ?>
+
+		<section class="value-tags">
+			<div class="container">
+				<div class="col-info">
+					<h3><?php echo $valueSection['value_driver_section_title'] ?></h3>
+					<p><?php echo $valueSection['value_driver_section_copy'] ?> </p>
+					<?php if($link){ ?><a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="button"><?php echo $link['title']; ?></a>	<?php } ?>
+				</div>
+				<div class="col-slider">
+					<?php if($valueSection['value_driver_section_labels']) { ?>
+					
+					<div class="insights-animation">
+						<div class="cards slide-in">
+						<?php foreach ($valueSection['value_driver_section_labels'] as $label) {?>
+							<div class="card">
+								<h4><?php echo $label['value_driver_label']; ?></h4>
+							</div>
+						<?php } ?>
+							
+							
+						</div>
+					</div>
+					<?php }; ?>
+				</div>
+			</div>
+		</section>
+	<?php } ?>	
+
+		<!-- STORY BOX -->
+
+		<section class="interior-story">
+			<div class="container">
+				<article class="brands">
+					<div class="col-left">
+						<img src="<?php bloginfo('template_url'); ?>/images/br-evite.png" alt="">
+						<p>Within 00 days of signing up for Solvvy, Optimizely instantly resolved 28% of its inbound support tickets.</p>
+						<a href="" class="button">Read Their Story</a>
+					</div>
+					<div class="col-right">
+						<figure class="image-slider">
+							<img src="<?php bloginfo('template_url'); ?>/images/image-slider-brand.jpg" alt="">
+						</figure>
+					</div>
+				</article>
+			</div>
+		</section>
+
+		<div class="cool-sep"></div>
+
+
+		<!-- SECTION METRICS -->
+		
+		<?php while(has_sub_field('section_5', 5)): ?>
+			<section class="metrics">
+				<div class="container">
+				<div class="col-left">					
+						<?php if(get_sub_field('sec5_text')): ?><h3 class="text-underlined"><?php the_sub_field('sec5_text'); ?></h3><?php endif; ?>
+						<?php if(get_sub_field('sec5_button')): ?>
+								<?php 
+								$link = get_sub_field('sec5_button');
+								if( $link ): ?>
+									<a href="<?php echo $link['url']; ?>" class="button"><?php echo $link['title']; ?></a>
+								<?php endif; ?>
+						<?php endif; ?>
+						 
+					<?php endwhile; ?>
+				</div>
+				<div class="col-right">
+					<ul>
+
+			<?php while(has_sub_field('section_5', 5)): ?>
+								
+			<?php if(get_sub_field('box_metrics')): ?>
+				<?php 
+					    
+					if( have_rows('box_metrics') ):
+
+					 while( have_rows('box_metrics') ) : the_row(); 
+					        
+					  ?>
+
+					    <li>
+							<div class="box-metric <?php the_sub_field('color_class'); ?>">
+								<small>
+									<i class="icon-monitor"></i>
+								<?php the_sub_field('icon_text'); ?>
+								</small>
+								<h3>
+									<?php the_sub_field('number'); ?>
+								</h3>
+								<p><?php the_sub_field('copy'); ?></p>
+							</div>
+						</li>
+							<?php
+								
+					    endwhile;
+
+					endif;
+
+					?>
+
+
+					</ul>
+				</div>
+			</div>
+		</section>
+		<?php endif; ?>
+							 
+		<?php endwhile; ?>
+
+		<!-- BOX BLUE FPO -->
+		<?php $assetSection = get_field('section_asset'); ?>
+		<?php if($assetSection){ ?>
+		<img src="<?php bloginfo('template_url'); ?>/images/waves-box-top.png" alt="" class="responsive block">
+		<div class="blue-box-waves">
+			<div class="container">
+				<div class="image">
+				<?php if($assetSection['asset_left_image']){ ?>
+				<?php
+				$attachment_id = $assetSection['asset_left_image'];
+				$size = "join_us_section";
+				$image = wp_get_attachment_image_src( $attachment_id, $size );
+				?>
+					<img src="<?php echo $image[0]; ?>" class="responsive" alt="">
+						
+				<?php } else { ?>
+					<img src="<?php bloginfo('template_url'); ?>/images/fpo.png" class="responsive" alt="">
+				<?php } ?>
+				</div>
+				<div class="info">
+					<h2><?php echo $assetSection['asset_title'] ?></h2>
+					<p><?php echo $assetSection['asset_description'] ?></p>
+					<a href="<?php echo $assetSection['asset_button_destination'] ?>" class="button-tn" target="_blank"><?php echo $assetSection['asset_button_text'] ?></a>
+				</div>
+			</div>
+		</div>
+		<img src="<?php bloginfo('template_url'); ?>/images/waves-box-bottom.png" alt="" class="responsive">
+		<?php } ?>
+
+
+<?php get_footer(); ?>
