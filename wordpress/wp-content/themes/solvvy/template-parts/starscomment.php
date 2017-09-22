234d88d27ee0854332db88460c45c41622831d02
Mariana Esteban <mariana@thebytery.com>
2017-09-22 14:03:19 -0300

    Careers
    https://bytery.mydonedone.com/issuetracker/projects/63362/issues/35

--- wordpress/wp-content/themes/solvvy/template-parts/starscomment.php
+++ wordpress/wp-content/themes/solvvy/template-parts/starscomment.php
@@ -3,22 +3,24 @@
 		<?php $testimonials = get_field('section_clients_testimonials'); ?>
 		<?php if($testimonials){ ?>	
 		<section class="box-gray box-testimonial">
-			<div class="container">
-				<ul class="formated-list">
-					<?php
-						$attachment_id = $testimonials['client_logo'];
-						$size = "client_logo";
-						$image = wp_get_attachment_image_src( $attachment_id, $size );
-					?>
-					<li><img src= "<?php echo $image[0]; ?>" alt="" class="responsive"></li>
-					<li><p><?php echo $testimonials['client_comment'] ?></p></li>
-					<li>
-						<?php for ($i = 1; $i <= $testimonials['stars_calification']; $i++) {  ?>
-						<i class="fa fa-star"></i>
-						<?php } ?>
-					</li>
-
-				</ul>
-			</div>
+			<a href="<?php echo $testimonials['testimonial_link'] ?>" target="_blank">
+				<div class="container">
+					<ul class="formated-list">
+						<?php
+							$attachment_id = $testimonials['client_logo'];
+							$size = "client_logo";
+							$image = wp_get_attachment_image_src( $attachment_id, $size );
+						?>
+						<li><img src= "<?php echo $image[0]; ?>" alt="" class="responsive"></li>
+						<li><p><?php echo $testimonials['client_comment'] ?></p></li>
+						<li>
+							<?php for ($i = 1; $i <= $testimonials['stars_calification']; $i++) {  ?>
+							<i class="fa fa-star"></i>
+							<?php } ?>
+						</li>
+				
+					</ul>
+				</div>
+			</a>
 		</section>
 		<?php } ?>
