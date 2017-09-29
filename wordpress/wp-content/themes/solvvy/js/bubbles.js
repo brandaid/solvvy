94ff84d2aef3b0120c4e144d660799a548e615f9
gacierno <german@thebytery.com>
2017-09-28 21:05:42 -0300

    About us bubbles hardcoded

--- wordpress/wp-content/themes/solvvy/js/bubbles.js
+++ wordpress/wp-content/themes/solvvy/js/bubbles.js
@@ -156,7 +156,7 @@ var BubbleScene = function(opts){
 	this.calculateSceneHeight = function(){
 		let cols = Math.floor(this._width / this.segment_size);
 		let rows = Math.ceil(this.bubbles.length / cols);
-		return this.segment_size * rows + 50;
+		return this.segment_size * rows + 150;
 	}
 
 	this.update = function(){
@@ -204,8 +204,11 @@ var BubbleScene = function(opts){
 					item.size = sizes[0];
 				}
 			}else{
-				var sizeOpt = (item.title.length + item.title.length > 250)? 0 : 1;
-				item.size = sizes[sizeOpt];
+				if(counter == 1 || counter == 4 || counter == 6 || counter == 9 || counter == 13 || counter == 16 || counter == 18 || counter == 21 || counter == 25|| counter == 28|| counter == 30|| counter == 33){
+					item.size = sizes[0];
+				}else{
+					item.size = sizes[1];
+				}
 			}
 			counter++;
 		});
@@ -300,8 +303,118 @@ var BubbleScene = function(opts){
 						bubble.position.y = Math.floor(Math.random() * (maxPosY - minPosY + 1)) + minPosY;;
 					}
 				}else{
-					bubble.position.x = Math.floor(Math.random() * (maxPosX - minPosX + 1)) + minPosX;
-					bubble.position.y = Math.floor(Math.random() * (maxPosY - minPosY + 1)) + minPosY;;
+					if(counter == 0){
+						bubble.position.x = 0;
+						bubble.position.y = 177;
+					}else if(counter == 1){
+						bubble.position.x = 357;
+						bubble.position.y = 12;
+					}else if(counter == 2){
+						bubble.position.x = 703;
+						bubble.position.y = 0;
+					}else if(counter == 3){
+						bubble.position.x = 1036;
+						bubble.position.y = 80;
+					}else if(counter == 4){
+						bubble.position.x = 50;
+						bubble.position.y = 445;
+					}else if(counter == 5){
+						bubble.position.x = 406;
+						bubble.position.y = 354;
+					}else if(counter == 6){
+						bubble.position.x = 708;
+						bubble.position.y = 291;
+					}else if(counter == 7){
+						bubble.position.x = 1031;
+						bubble.position.y = 481;
+					}else if(counter == 8){
+						bubble.position.x = 0;
+						bubble.position.y = 833;
+					}else if(counter == 9){
+						bubble.position.x = 405;
+						bubble.position.y = 664;
+					}else if(counter == 10){
+						bubble.position.x = 772;
+						bubble.position.y = 680;
+					}else if(counter == 11){
+						bubble.position.x = 1062;
+						bubble.position.y = 789;
+					}else if(counter == 12){
+						bubble.position.x = 0;
+						bubble.position.y = 177 + 960;
+					}else if(counter == 13){
+						bubble.position.x = 357;
+						bubble.position.y = 12 + 960;
+					}else if(counter == 14){
+						bubble.position.x = 703;
+						bubble.position.y = 0 + 960;
+					}else if(counter == 15){
+						bubble.position.x = 1036;
+						bubble.position.y = 80 + 960;
+					}else if(counter == 16){
+						bubble.position.x = 50;
+						bubble.position.y = 445 + 960;
+					}else if(counter == 17){
+						bubble.position.x = 406;
+						bubble.position.y = 354 + 960;
+					}else if(counter == 18){
+						bubble.position.x = 708;
+						bubble.position.y = 291 + 960;
+					}else if(counter == 19){
+						bubble.position.x = 1031;
+						bubble.position.y = 481 + 960;
+					}else if(counter == 20){
+						bubble.position.x = 0;
+						bubble.position.y = 833 + 960;
+					}else if(counter == 21){
+						bubble.position.x = 405;
+						bubble.position.y = 664 + 960;
+					}else if(counter == 22){
+						bubble.position.x = 772;
+						bubble.position.y = 680 + 960;
+					}else if(counter == 23){
+						bubble.position.x = 1062;
+						bubble.position.y = 789 + 960;
+					}else if(counter == 24){
+						bubble.position.x = 0;
+						bubble.position.y = 177 + 960 + 960;
+					}else if(counter == 25){
+						bubble.position.x = 357;
+						bubble.position.y = 12 + 960 + 960;
+					}else if(counter == 26){
+						bubble.position.x = 703;
+						bubble.position.y = 0 + 960 + 960;
+					}else if(counter == 27){
+						bubble.position.x = 1036;
+						bubble.position.y = 80 + 960 + 960;
+					}else if(counter == 28){
+						bubble.position.x = 190;
+						bubble.position.y = 2265;
+					}else if(counter == 29){
+						bubble.position.x = 686;
+						bubble.position.y = 354 + 960 + 960;
+					}else if(counter == 30){
+						bubble.position.x = 708;
+						bubble.position.y = 291 + 960 + 960;
+					}else if(counter == 31){
+						bubble.position.x = 1031;
+						bubble.position.y = 481 + 960 + 960;
+					}else if(counter == 32){
+						bubble.position.x = 0;
+						bubble.position.y = 833 + 960 + 960;
+					}else if(counter == 33){
+						bubble.position.x = 405;
+						bubble.position.y = 664 + 960 + 960;
+					}else if(counter == 34){
+						bubble.position.x = 772;
+						bubble.position.y = 680 + 960 + 960;
+					}else if(counter == 35){
+						bubble.position.x = 1062;
+						bubble.position.y = 789 + 960 + 960;
+					}else {
+						bubble.position.x = Math.floor(Math.random() * (maxPosX - minPosX + 1)) + minPosX;
+						bubble.position.y = Math.floor(Math.random() * (maxPosY - minPosY + 1)) + minPosY;;
+					}
 				}
 
 			}
@@ -383,26 +496,141 @@ var BubbleScene = function(opts){
 						
 				}else{
 					if(current_segment.element === true){
+						
 						self.segments.segments.forEach(function(other_segment){
-							if(other_segment.element == true){
-								if(other_segment.c == current_segment.c -1 || other_segment.c == current_segment.c || other_segment.c == current_segment.c +1){
-									if(other_segment.r == current_segment.r -1 || other_segment.r == current_segment.r || other_segment.r == current_segment.r +1){
-										var connector = new Nodeline({
-											start : current_segment.bubble.center(),
-											end : other_segment.bubble.center(),
-											width : 2
-										});
-										var valid = true;
-										self.connectorsAll.forEach(function(other_connector){
-											if(connector.start.x == other_connector.end.x && connector.start.y == other_connector.end.y){
-												valid = false;
-											};
-										});
-										if(valid){self.connectorsAll.push(connector);};
-									}
+							var create_con = false;
+							
+							if(counter == 0){
+								if(other_counter == 1 || other_counter == 5){
+									create_con = true;
+								}
+							}else if(counter == 1){
+								if(other_counter == 2 || other_counter == 5 || other_counter == 6){
+									create_con = true;
+								}
+							}else if(counter == 2){
+								if(other_counter == 3 ){
+									create_con = true;
+								}
+							}else if(counter == 3){
+								if(other_counter == 7 || other_counter == 6){
+									create_con = true;
+								}
+							}else if(counter == 4){
+								if(other_counter == 0 || other_counter == 5){
+									create_con = true;
+								}
+							}else if(counter == 5){
+								if(other_counter == 6 || other_counter == 9){
+									create_con = true;
+								}
+							}else if(counter == 6){
+								if(other_counter == 2 || other_counter == 7 || other_counter == 9){
+									create_con = true;
+								}
+							}else if(counter == 7){
+								if(other_counter == 10 || other_counter == 11){
+									create_con = true;
+								}
+							}else if(counter == 8){
+								if(other_counter == 4 || other_counter == 12 || other_counter == 9){
+									create_con = true;
+								}
+							}else if(counter == 9){
+								if(other_counter == 4 || other_counter == 13){
+									create_con = true;
+								}
+							}else if(counter == 10){
+								if(other_counter == 9 || other_counter == 6 || other_counter == 11 || other_counter ==14){
+									create_con = true;
+								}
+							}else if(counter == 11){
+								if(other_counter == 15){
+									create_con = true;
+								}
+							}else if(counter == 12){
+								if(other_counter == 13 || other_counter == 16 || other_counter == 17){
+									create_con = true;
+								}
+							}else if(counter == 13){
+								if(other_counter == 14 || other_counter == 17 || other_counter == 18){
+									create_con = true;
+								}
+							}else if(counter == 14){
+								if(other_counter == 15 ){
+									create_con = true;
+								}
+							}else if(counter == 15){
+								if(other_counter == 19 || other_counter == 18){
+									create_con = true;
+								}
+							}else if(counter == 16){
+								if( other_counter == 17){
+									create_con = true;
+								}
+							}else if(counter == 17){
+								if(other_counter == 18 || other_counter == 21){
+									create_con = true;
+								}
+							}else if(counter == 18){
+								if(other_counter == 14 || other_counter == 19 || other_counter == 21){
+									create_con = true;
+								}
+							}else if(counter == 19){
+								if(other_counter == 22 || other_counter == 23){
+									create_con = true;
+								}
+							}else if(counter == 20){
+								if(other_counter == 16 || other_counter == 24 || other_counter == 21){
+									create_con = true;
+								}
+							}else if(counter == 21){
+								if(other_counter == 16 || other_counter == 25){
+									create_con = true;
+								}
+							}else if(counter == 22){
+								if(other_counter == 21 || other_counter == 18 || other_counter == 23 || other_counter == 26){
+									create_con = true;
+								}
+							}else if(counter == 23){
+								if(other_counter == 27){
+									create_con = true;
+								}
+							}else if(counter == 24){
+								if(other_counter == 25 || other_counter == 28){
+									create_con = true;
+								}
+							}else if(counter == 25){
+								if(other_counter == 26 || other_counter == 28 || other_counter == 29){
+									create_con = true;
+								}
+							}else if(counter == 26){
+								if(other_counter == 27 || other_counter == 29){
+									create_con = true;
+								}
+							}else if(counter == 27){
+								if( other_counter == 29){
+									create_con = true;
+								}
+							}else if(counter == 28){
+								if( other_counter == 29){
+									create_con = true;
+								}
+							}else if(counter == 29){
+								if(other_counter == 30 || other_counter == 33){
+									
 								}
 							}
 
+							if(create_con){
+								var connector = new Nodeline({
+									start : current_segment.bubble.center(),
+									end : other_segment.bubble.center(),
+									width : 2
+								});
+								self.connectorsAll.push(connector);
+							}
+							other_counter ++;
 						});
 
 					}
